493a3964dd34dd3760b5d4edbde0d03eae2c6599
ParthVijay13 <parthvijay13@gmail.com>
2024-04-01 14:48:39 +0530

    Form completion done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import LoginForm from './form';
+import { jwtDecode } from "jwt-decode";
 function App() {
   return (
     <div className="App">
+
+      <LoginForm />
       
+
       
     </div>
   );
---
+++ src/form.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { jwtDecode } from "jwt-decode";
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div className="form-container">
+      <h2>Login Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <h2>or</h2>
+      <div className='google-auth'>
+      <GoogleOAuthProvider clientId="416100393732-u4ha5qcupp4a7dkeughs512938bujrr1.apps.googleusercontent.com"> 
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            const decoded = jwtDecode(credentialResponse.credential);
+            console.log(decoded);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
+      </GoogleOAuthProvider>
+      </div>
+      
+    </div>
+  );
+};
+
+export default LoginForm;
